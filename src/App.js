a421e2c52c01c7ec135fd34503404470768f5767
Oviya <ammuoviya19@gmail.com>
2023-10-12 22:00:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Card from './parent_components/Card';
+function App() {
+  return (
+    <div className='container'>
+      <div className='title'>
+        <h1>Quickkiggy</h1>
+      </div>
+    <div className="App">
+      <Card/>
+    </div>
+    </div>
+  );
+}
+
+export default App;
